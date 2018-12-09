0e896d5843ecd975227cb585e5488f32d4cb7f57
Alice <5541037@qq.com>
2018-12-09 15:09:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let fs = require('fs');
+function witerFile (path, data) {
+  fs.writeFile(path, data,function(err){
+      if(err){
+        console.error(err);
+      } else {
+        console.log('写入成功');
+      }
+    })
+}
 module.exports = (api, projectOptions) => {
   api.registerCommand('version', {
   	description: 'version plugin for vue cli 3',
@@ -5,6 +15,25 @@ module.exports = (api, projectOptions) => {
   	options: {}
   }, (args) => {
   	let options = projectOptions.pluginOptions.demoOptions
-  	console.log('options', options)
+    const {path, env, versionDirectory, name} = options
+    const DIR = `${path}/${versionDirectory}`
+    const NAME = name || 'version'
+    const VERSION = `${DIR}/${NAME}.json`
+    fs.exists(DIR, function (exists) {
+     if(exists){
+       // 创建 version.js
+       console.log('检查 verson')
+       witerFile(VERSION, `{"${NAME}":${env.VERSION}}`)
+     }else {
+       fs.mkdir(`${path}/static`, function() {
+         console.log('创建 static')
+         witerFile(VERSION, `{"${NAME}":${env.VERSION}}`)
+       });  
+       
+     }
+    });
   })
+}
+module.exports.defaultModes = {
+  build: 'production'
 }
